fa68127fdf7feeb7a3baf5ab7c34d820d6337a6e
reduviidae <wiljago@gmail.com>
2019-03-06 15:00:57 -0500

    db:seed includes logged in users

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,5 @@
 class Chat < ApplicationRecord
   has_many :messages, dependent: :destroy
+  has_many :user_conversations
   belongs_to :friendship
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :chats, only: [:index, :create]
-  resources :users, only: [:index, :show, :create, :new]
+  resources :users, only: [:index, :show, :create]
   resources :messages, only: [:index, :create]
   resources :friendships, only: [:index, :create]
+  resources :user_conversations, only: [:index, :create, :show]
   post '/login', to: 'auth#create'
   post '/signup', to: 'users#new'
   get '/profile', to: 'users#profile'
