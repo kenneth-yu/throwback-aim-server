eef0b8b96e2895020e747efe374ab41088b26f66
reduviidae <wiljago@gmail.com>
2019-03-07 07:48:26 -0500

    commit before revert

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,37 +6,11 @@ class ChatsController < ApplicationController
 
   def create
 
-    chat = Chat.new(chat_params)
+    chat = Chat.new()
 
     if chat.save
-
-        ownership1 = UserConversation.new()
-        ownership1.chat_id = chat.id
-        ownership1.user_id = params["sender_id"]
-        ownership1.save
-        ownership2 = UserConversation.new()
-        ownership2.chat_id = chat.id
-        ownership2.user_id = params["receiver_id"]
-        ownership2.save
-
-        serialized_data = ActiveModelSerializers::Adapter::Json.new(
-            ChatSerializer.new(chat)
-        ).serializable_hash
-
-
-        ActionCable.server.broadcast(
-            "current_user_#{current_user.id}",
-            serialized_data
-        )
-
-        ActionCable.server.broadcast(
-            "current_user_#{params["receiver_id"]}",
-            serialized_data
-        )
-
-        head :ok
+      render :json => @chat, each_serializer: ChatSerializer
     end
-    render :json => @chat, each_serializer: ChatSerializer
   end
 
   def show
